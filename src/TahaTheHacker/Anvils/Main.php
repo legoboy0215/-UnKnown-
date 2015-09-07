661a63167b469d03c9ad3707e4d3b943ef5c772e
MCPEPIG <mcpepig123@gmail.com>
2015-09-06 20:52:51 -0700

    :P

--- src/TahaTheHacker/Anvils/Main.php
+++ src/TahaTheHacker/Anvils/Main.php
@@ -3,26 +3,23 @@ namespace TahaTheHacker\Anvils;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\plugin\PluginLoader;
-use pocketmine\player;
+use pocketmine\Player;
 use pocketmine\event\player\PlayerItemHeldEvent;
-use pocketmine\item\item;
+use pocketmine\item\Item;
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerCommandPreprocessEvent;
 use pocketmine\utils\TextFormat;
 use pocketmine\command\ConsoleCommandSender;
 
  class Main extends PluginBase implements Listener {
   
    public function onEnable(){
-   $this->getServer()->getPluginManager()->registerEvents($this, $this);
-   $this->getLogger()->info(TextFormat::DARK_GREEN . "✔ §c+§6Snowball§cs§2 Enabled");
-       	}
+     $this->getServer()->getPluginManager()->registerEvents($this, $this);
+     $this->getLogger()->info(TextFormat::DARK_GREEN . "✔ §c+§6Snowball§cs§2 Enabled");
+   }
    public function ItemHeld(PlayerItemHeldEvent $event) {
-  $item = $event->getItem();
-  $id = $item->getId();
-  $player = $event->getPlayer();
-  
- 
-  }//this is of the ItemHeld function;
+     $item = $event->getItem();
+     $id = $item->getId();
+     $player = $event->getPlayer();   
+   }//this is of the ItemHeld function;
  }//this is of the Class Main;
   
